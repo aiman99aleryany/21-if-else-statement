73241e4b49e67e30a4ce10c028ba0fe1db2ee694
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-17 04:03:28 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,6 @@
+// Create a variable called password. If the password length is between 4 and 6, print the message Password length is correct, otherwise print the message Password not valid.
+let password = "aiman";
+
+password.length < 6 && password.length > 4
+  ? console.log("Password length is correct")
+  : console.log("Password not valid");
